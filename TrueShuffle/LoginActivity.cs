e4422959584b8277654de679d79d0d50be966b34
Cameron Salisbury <cam2772@gmail.com>
2019-11-11 14:19:54 +1300

    Initial commit

---
+++ TrueShuffle/LoginActivity.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Util;
+using Android.Widget;
+using Com.Spotify.Sdk.Android.Authentication;
+
+namespace TrueShuffle
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class LoginActivity : AppCompatActivity
+    {
+        private const string ClientId = "443f91927a354f6ea0f66c98629e8ebf";
+        private const string RedirectUri = "com.cameronsalisbury.trueshuffle://callback";
+        private const int RequestCode = 5684;
+        private const string Scopes = "playlist-read-private playlist-modify-public playlist-modify-private user-read-private";
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_login);
+
+            Button loginButton = FindViewById<Button>(Resource.Id.login_button);
+            loginButton.Click += LoginOnClick;
+
+            // if token already saved, go to main
+//            if (GetSharedPreferences("SPOTIFY", 0).GetString("token", "") != "")
+//            { 
+//                Intent i = new Intent(this, typeof(MainActivity));
+//                StartActivity(i);
+//            }
+        }
+
+        private void LoginOnClick(object sender, EventArgs eventArgs)
+        {
+            AuthenticationRequest.Builder builder =
+                new AuthenticationRequest.Builder(ClientId, AuthenticationResponse.Type.Token, RedirectUri);
+            builder.SetScopes(new[] { Scopes });
+            AuthenticationRequest request = builder.Build();
+            AuthenticationClient.OpenLoginActivity(this, RequestCode, request);
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent intent)
+        {
+            int resultCodeInt = (int)resultCode;
+            resultCodeInt += 0;
+
+            if (requestCode != RequestCode) return;
+
+            AuthenticationResponse response = AuthenticationClient.GetResponse(resultCodeInt, intent);
+            if (response.GetType() == AuthenticationResponse.Type.Token)
+            {
+                Log.Debug("SpotifyAuth", $"Auth token: {response.AccessToken}");
+
+                ISharedPreferencesEditor editor = GetSharedPreferences("SPOTIFY", 0).Edit();
+                editor.PutString("token", response.AccessToken);
+                editor.Commit();
+
+                Intent i = new Intent(this, typeof(MainActivity));
+                StartActivity(i);
+            }
+            else if (response.GetType() == AuthenticationResponse.Type.Error)
+            {
+                Log.Debug("SpotifyAuth", $"Auth error: {response.Error}");
+            }
+            else
+            {
+                Log.Debug("SpotifyAuth", "Auth failed");
+            }
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ TrueShuffle/MainActivity.cs
@@ -0,0 +1,88 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Models;
+using Xamarin.Essentials;
+using ContextThemeWrapper = Android.Support.V7.View.ContextThemeWrapper;
+
+namespace TrueShuffle
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        private SpotifyWebAPI api;
+        private string userId;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            string token = GetSharedPreferences("SPOTIFY", 0).GetString("token", "");
+            if (token != "")
+            {
+                api = new SpotifyWebAPI {AccessToken = token, TokenType = "Bearer"};
+
+                userId = api.GetPrivateProfile().Id;
+            }
+            else
+            {
+                Intent i = new Intent(this, typeof(LoginActivity));
+                i.SetFlags(ActivityFlags.NewTask | ActivityFlags.ClearTask);
+                StartActivity(i);
+            }
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            LoadPlaylists();
+        }
+
+        private void LoadPlaylists()
+        {
+            Paging<SimplePlaylist> playlists = api.GetUserPlaylists(userId, 10);
+
+            GridLayout parentLayout = FindViewById<GridLayout>(Resource.Id.playlist_display);
+            foreach (SimplePlaylist playlist in playlists.Items)
+            {
+                TextView textView = new TextView(new ContextThemeWrapper(this, Resource.Style.PlaylistLabel))
+                {
+                    Text = playlist.Name,
+                    LayoutParameters = new GridLayout.LayoutParams(GridLayout.InvokeSpec(GridLayout.Undefined, 0f),
+                        GridLayout.InvokeSpec(GridLayout.Undefined, 1f))
+                };
+                textView.LayoutParameters.Width = 0;
+
+                Button button = new Button(new ContextThemeWrapper(this, Resource.Style.PlaylistButton))
+                {
+                    LayoutParameters = new GridLayout.LayoutParams(GridLayout.InvokeSpec(GridLayout.Undefined, 0f),
+                        GridLayout.InvokeSpec(GridLayout.Undefined, 0f))
+                };
+                button.LayoutParameters.Width = ViewGroup.LayoutParams.WrapContent;
+                button.LayoutParameters.Height = ViewGroup.LayoutParams.WrapContent;
+                button.Gravity = GravityFlags.Center;
+
+                parentLayout.AddView(textView);
+                parentLayout.AddView(button);
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions,
+            [GeneratedEnum] Permission[] grantResults)
+        {
+            Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
